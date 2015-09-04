27fdfffed8c2cc24ecbc15da9f8fd248905e9ace
Kenta Onimura <kentaonex@gmail.com>
2015-09-03 21:19:53 -0700

    added terminal and brew commands etc

--- install.sh
+++ install.sh
@@ -6,3 +6,15 @@ do
     cp -v "$HOME/.$entry" "$HOME/.$entry\.saved"
     cp -v "config/$entry" "$HOME/.$entry"
 done
+
+# Download brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Download Vim Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+brew install ack
+brew install tmux
+brew install reattach-to-user-namespace
+brew install ctags
